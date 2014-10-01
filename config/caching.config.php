874f80a781df779d5f2085a893dd3297632d5649
Andrew Wittrock <andy@wittrock.us>
2014-10-01 02:02:55 -0500

    Add page caching

---
+++ config/caching.config.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+    'strokercache' => array(
+        'strategies' => array(
+            'enabled' => array(
+                'StrokerCache\Strategy\RouteName' => array(
+                    'routes' => array(
+                        'gallery',
+                    ),
+                ),
+            ),
+        )
+    )
+);
