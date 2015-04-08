21cd9520a9247169d8f8b6d74aa3a60974d866e9
Andrew Wittrock <andy@wittrock.us>
2015-04-08 10:28:25 -0500

    Extend char column lengths

--- src/ATPGallery/Module.php
+++ src/ATPGallery/Module.php
@@ -13,7 +13,7 @@ class Module extends \ATP\Module
 			"CREATE TABLE `atpgallery_images` (
 				`id` int(11) NOT NULL AUTO_INCREMENT,
 				`title` char(64) COLLATE utf8_unicode_ci DEFAULT NULL,
-				`url` char(32) COLLATE utf8_unicode_ci NOT NULL,
+				`url` char(64) COLLATE utf8_unicode_ci NOT NULL,
 				`post_date` date DEFAULT NULL,
 				`image_file` char(255) COLLATE utf8_unicode_ci DEFAULT NULL,
 				`thumbnail_file` char(255) COLLATE utf8_unicode_ci DEFAULT NULL,
